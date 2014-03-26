e36e655f487773b6fec105cba0da1045496af223
Rick Hall <rickhall2000@hotmail.com>
2014-03-26 10:31:16 -0400

    refactored routes/resource defs.  liberator rocks.

--- src/backup_scheduler/routes/apihome.clj
+++ src/backup_scheduler/routes/apihome.clj
@@ -9,43 +9,22 @@
   :allowed-methods [:get]
   :available-media-types ["text/plain"])
 
-(defresource get-folders
+(defresource folders
+  :allowed-methods [:get :post]
   :handle-ok (m/get-folders)
-  :allowed-methods [:get]
-  :etag "fixed-etag"
-  :available-media-types ["application/edn"])
-
-(defresource get-folder
-  :handle-ok (fn [context]
-               (let [params (get-in context [:request :route-params])]  (m/get-folder (read-string (:id params))) ))
-  :allowed-methods [:get]
-  :etag "fixed-etag"
-  :available-media-types ["application/edn"])
-
-(defresource create-folder
   :post! (fn [context]
            (let [body (slurp (get-in context [:request :body]))]
                (m/add-folder body)))
-  :allowed-methods [:post]
   :etag "fixed-etag"
-  :available-media-types ["application/edn" "Text/plain"])
+  :available-media-types ["application/edn"])
 
-(defresource update-folder
-  :handle-ok "update folder"
-  :allowed-methods [:put]
+(defresource folder [id]
+  :allowed-methods [:get :put :delete]
+  :handle-ok  (m/get-folder (read-string id))
   :etag "fixed-etag"
-  :available-media-types ["Text/plain"])
-
-(defresource delete-folder
-  :handle-ok "delete folder"
-  :allowed-methods [:delete]
-  :etag "fixed-etag"
-  :available-media-types ["Text/plain"])
+  :available-media-types ["application/edn"])
 
 (defroutes api-routes
   (GET "/api" request api-home)
-  (GET "/api/folders" request get-folders)
-  (GET "/api/folders/:id" request get-folder)
-  (POST "/api/folders" request create-folder)
-  (PUT "/api/folders/:id" request update-folder)
-  (DELETE "/api/folders/:id" request delete-folder))
+  (ANY "/api/folders" request folders)
+  (ANY "/api/folders/:id" [id] (folder id)))
